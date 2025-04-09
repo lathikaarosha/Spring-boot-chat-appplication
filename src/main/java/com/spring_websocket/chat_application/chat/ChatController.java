7f0a55b41e2836a81bc36910b554ad33ae1f7b26
LATHIKAJ.OU <LATHIKAJ.OU@mobitel.lk>
2025-04-09 20:48:11 +0530

    create websocket server and msg endpoint

---
+++ src/main/java/com/spring_websocket/chat_application/chat/ChatController.java
@@ -0,0 +1,27 @@
+package com.spring_websocket.chat_application.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage chatMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor simpMessageHeaderAccessor){
+
+        // add username in a web socket session
+        simpMessageHeaderAccessor.getSessionAttributes().put("userName",chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
---
+++ src/main/java/com/spring_websocket/chat_application/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.spring_websocket.chat_application.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType messageType;
+
+}
